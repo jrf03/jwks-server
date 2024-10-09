a25f9c5beb0b87b46cb9a52dd4eb31f4b19e7c0b
jrf03 <jacobrfranks@gmail.com>
2024-10-08 19:06:18 -0500

    Update test.py
    Fixed indentation that cased the server to not run

--- test.py
+++ test.py
@@ -94,5 +94,5 @@ def verify():
     except Exception as e:
         return jsonify({"message": str(e)}), 400
 
-    if __name__ == '__main__':
-        app.run(host='0.0.0.0', port=5001)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001)
