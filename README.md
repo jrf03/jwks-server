08c6267483283ea57ee0ca6c0c879cc26037080d
jrf03 <jacobrfranks@gmail.com>
2024-10-27 20:37:45 -0500

    Reworked app.py to use SQLite database

--- README.md
+++ README.md
@@ -4,4 +4,7 @@
 # how to run on linux
     Open the terminal.
     Change the directory to the Flask project folder.
-    Type "python3 server.py"
+    Create/start the virtual environment.
+    If needed: pip install Flask
+    If needed: pip install jwcrypto
+    Type "flask run" to start the server.
--- app.py
+++ app.py
@@ -2,46 +2,70 @@ from flask import Flask, json, jsonify, request
 from jwcrypto import jwk, jwt
 from datetime import datetime, timedelta
 import time
+import sqlite3
 
 app = Flask(__name__)
 
-key_store = {}
-expired_key_store = {}
+database = 'keys.db'
+
+def init_db():
+    with sqlite3.connect(database) as conn:
+        cursor = conn.cursor()
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS keys (
+                kid TEXT PRIMARY KEY,
+                private_key TEXT NOT NULL
+                expiry TIMESTAMP NOT NULL
+            )
+        ''')
+        conn.commit()
+
+init_db()
 expiry_duration = timedelta(minutes=30)
 
 def generate_key():
     key = jwk.JWK.generate(kty = 'RSA', size=2048)
     return key
 
+def store_key(kid, private_key, expiry):
+    with sqlite3.connect(database) as conn:
+        cursor = conn.cursor()
+        cursor.execute('''
+            INSERT OR REPLACE INTO keys (kid, private_key, expiry) VALUES (?, ?, ?)
+        ''', (kid, private_key.export_to_pem(private_key=True).decode(), expiry))
+        conn.commit()
+
 def get_key():
-    curr_time = datetime.utcnow()
-    if not key_store or curr_time > list(key_store.values())[0]['expiry']:
+    curr_time = datetime.now(datetime.timezone.utc)
+    with sqlite3.connect(database) as conn:
+        cursor = conn.cursor()
+        cursor.execute('SELECT kid, private_key, expiry FROM keys WHERE expiry > ?', (curr_time,))
+        row = cursor.fetchone()
+
+    if row is None:
         private_key = generate_key()
-        kid = str(int(time.time()))
+        kid = (str(int(time.time())))
         expire_time = curr_time + expiry_duration
 
-        key_store.clear()
-        key_store[kid] = {
-            'key': private_key,
-            'expiry': expire_time
-        }
+        store_key(kid, private_key, expire_time)
+        return kid, {'key': private_key, 'expiry': expire_time}
     
-    kid = list(key_store.keys())[0]
-    return kid, key_store[kid]
+    kid, private_key_str, expiry = row
+    private_key = jwk.JWK.from_pem(private_key_str.encode())
+    return kid, {'key': private_key, 'expiry': expire_time}
 
 @app.route('/jwks.json')
 def jwks():
     keys = []
 
-    for kid, key_data in key_store.items():
-        jwk_public = key_data['key'].export_public(as_dict=True)
-        jwk_public['kid'] = kid
-        keys.append(jwk_public)
-    
-    for kid, key_data in expired_key_store.items():
-        jwk_public = key_data['key'].export_public(as_dict=True)
-        jwk_public['kid'] = kid
-        keys.append(jwk_public)
+    with sqlite3.connect(database) as conn:
+        cursor = conn.cursor()
+        cursor.execute('SELECT kid, private_key FROM keys')
+
+        for kid, private_key_str in cursor.fetchall():
+            jwk_public = jwk.JWK.from_pem(private_key_str.encode().export_public(as_dict=True))
+            jwk_public['kid'] = kid
+            keys.append(jwk_public)
 
     jwks = {"keys": keys}
     return jsonify(jwks)
@@ -61,8 +85,16 @@ def generate_jwt(private_key, kid, expired=False):
 def authenticate():
     use_expired_key = request.args.get('use_expired_key', 'false').lower() == 'true'
 
-    if use_expired_key and expired_key_store:
-        kid, key_data = next(iter(expired_key_store.items()))
+    if use_expired_key:
+        with sqlite3.connect(database) as conn:
+            cursor = conn.cursor()
+            cursor.execute('SELECT kid, private_key FROM keys WHERE expiry <= ?', (datetime.now(datetime.utc.now),))
+            expired_row = cursor.fetchone()
+            if expired_row:
+                kid, private_key_str = expired_row
+                key_data = {'key': jwk.JWK.from_pem(private_key_str.encode())}
+            else:
+                return jsonify({"message": "No expired keys available"}), 400
     else:
         kid, key_data = get_key()
     
